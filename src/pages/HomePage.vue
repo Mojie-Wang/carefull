6df9e7e3c2cbcb3254f4d01827f42a1aa8a2a772
RiversMoon <911289836@qq.com>
2026-03-20 21:13:43 +0800

    applyCard.vue优化

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,15 +2,27 @@
   <div class="pages">
     <List />
     <Banner />
+    <h6 class="applyCardTitle">
+      — Engineered for Advanced Protection and Customization —
+    </h6>
+    <ApplyCard />
   </div>
 </template>
 <script setup>
+import ApplyCard from "@/components/ApplyCard.vue";
 import Banner from "@/components/Banner.vue";
 import List from "@/components/List.vue";
 </script>
 <style scoped lang="less">
-.title {
-  font-size: 24px;
+.applyCardTitle {
+  // font-size: 24px;
   font-weight: 600;
+  font-family: var(--font-montserrat);
+  // font-size: 39px;
+  font-size: clamp(15px, 4vw, 39px);
+  color: #000000;
+  line-height: 48px;
+  margin: clamp(25px, 8vw, 108px) auto clamp(25px, 8vw, 115px);
+  text-align: center;
 }
 </style>
